cf671af3b22e7f48f23e4c403557d1f06c8c09e5
Péter Biró <biro.peter.hu@gmail.com>
2018-01-16 11:43:40 +0100

    first dev commit

--- app/src/main/jni/src/main.cpp
+++ app/src/main/jni/src/main.cpp
@@ -20,45 +20,9 @@ int main( int argc, char* args[] )
     }
     else
     {
-        //Load media
-        if( !loadMedia() )
-        {
-            SDL_Log( "Failed to load media!\n" );
-        }
-        else
-        {
-            //Main loop flag
-            bool quit = false;
-
-            //Event handler
-            SDL_Event e;
-
-            //While application is running
-            while( !quit )
-            {
-                //Handle events on queue
-                while( SDL_PollEvent( &e ) != 0 )
-                {
-                    //User requests quit
-                    if( e.type == SDL_QUIT )
-                    {
-                        quit = true;
-                    }
-                }
-
-                //Clear screen
-                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-                SDL_RenderClear( gRenderer );
-
-                //Render splash
-                gSplashTexture.render( ( gScreenRect.w - gSplashTexture.getWidth() ) / 2, ( gScreenRect.h - gSplashTexture.getHeight() ) / 2 );
-
-                //Update screen
-                SDL_RenderPresent( gRenderer );
-            }
-        }
+       SDL_Log("SDL started\n");
     }
-
+    SDL_Delay(3000);
     //Free resources and close SDL
     close();
 

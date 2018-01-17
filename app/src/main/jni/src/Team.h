59bdd72908d676c49ba50268fb42ca713bfbdffe
Péter Biró <biro.peter.hu@gmail.com>
2018-01-17 10:51:24 +0100

    Team.h created

---
+++ app/src/main/jni/src/Team.h
@@ -0,0 +1,32 @@
+//
+// Created by birop on 17/01/2018.
+//
+
+#ifndef FIVEBALLORGANIZER_TEAM_H
+#define FIVEBALLORGANIZER_TEAM_H
+
+#include <string>
+
+class Team {
+public:
+
+    Team(std::string name, std::string category);
+    Team(std::string name, std::string category, std::string email);
+    ~Team();
+
+    std::string getName();
+    void setName(std::string newName);
+    std::string getCategory();
+    void  setCategory(std::string newCategory);
+    std::string getEmail();
+    void setEmail(std::string newEmail);
+
+
+private:
+    std::string name;
+    std::string category;
+    std::string email;
+};
+
+
+#endif //FIVEBALLORGANIZER_TEAM_H
